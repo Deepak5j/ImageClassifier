d29cc708da8c697974c7cae9a209051ed7c59c10
deepak5j <deepakgautama5j@gmail.com>
2019-08-31 20:06:58 +0530

    replacing img with live webcam 🎥

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
+const webcamElement = document.getElementById('webcam');
+
 let net;
 
 async function app() {
+	console.log('Loading mobilenet..');
+	// Load the model.
+	net = await mobilenet.load();
+	console.log('Sucessfully loaded model');
 
-  console.log('Loading mobilenet..');
+	// Make a prediction through the model on image from webcam.
+	await setupWebcam();
+	while (true) {
+	  	const result = await net.classify(webcamElement);
+	  	document.getElementById('console').innerText = `
+	  	prediction: ${result[0].className}\n
+	  	probability: ${result[0].probability}
+	  	`;
+	    // Give some breathing room by waiting for the next animation frame to
+	    // fire.
+	    await tf.nextFrame();
+	}
+}
 
-  // Load the model.
-  net = await mobilenet.load();
-  console.log('Sucessfully loaded model');
-
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result[0]);
-
-  alert(result[0].className);
-  alert(result[0].probability);
+async function setupWebcam() {
+	return new Promise((resolve, reject) => {
+		const navigatorAny = navigator;
+		navigator.getUserMedia = navigator.getUserMedia ||
+		navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+		navigatorAny.msGetUserMedia;
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({video: true},
+				stream => {
+					webcamElement.srcObject = stream;
+					webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+				},
+				error => reject());
+		} else {
+			reject();
+		}
+	});
 }
 
 app();
